450b16ad531f6558a0c73b0fbaecb0c4f4ba2945
Ashley M. Jones <ashleyj38@gmail.com>
2016-04-12 10:11:25 -0700

    Add display components for GridLayout

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ed/2001ab0bd10000161fd5df4cb66b80f9
@@ -0,0 +1,21 @@
+class Day {
+	int date;
+	char weekday;
+}
+
+class Week {
+	int weekNum;
+	Day week[];
+}
+
+class Month {
+	Week month[];
+}
+
+class Year {
+	Month year[];
+}
+
+public class Calendar {
+	
+}
--- Calendar/src/Calendar.java
+++ Calendar/src/Calendar.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 class Day {
 	int date;
 	char weekday;
@@ -16,6 +19,78 @@ class Year {
 	Month year[];
 }
 
-public class Calendar {
+public class Calendar extends JFrame {
 	
+	private JPanel p;
+	private int days = 30;
+	GridLayout experiment = new GridLayout(0,7);
+	
+	public Calendar() {
+		
+		
+		
+	}
+	
+	public void setButtons(final Container p) {
+		
+		final JPanel components = new JPanel();
+		components.setLayout(experiment);
+		JPanel headings = new JPanel();
+		headings.setLayout(experiment);
+		//p.setVisible(true);
+		
+		for (int count = 0; count < days; count++)
+		{
+			components.add(new JButton("Button1"));
+		
+		/*components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));
+		components.add(new JButton("Button1"));*/
+		}
+		
+		headings.add(new Label("Sunday"));
+		headings.add(new Label("Monday"));
+		headings.add(new Label("Tuesday"));
+		headings.add(new Label("Wednesday"));
+		headings.add(new Label("Thursday"));
+		headings.add(new Label("Friday"));
+		headings.add(new Label("Saturday"));
+		
+		
+		experiment.setHgap(0);
+		experiment.setVgap(0);
+		experiment.layoutContainer(components);
+		
+		p.add(headings, BorderLayout.NORTH);
+		p.add(components, BorderLayout.CENTER);
+		
+	}
+	
+	private static void showGUI() {
+		Calendar frame = new Calendar();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setButtons(frame.getContentPane());
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new GridLayoutTest();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				showGUI();
+			}
+		});
+	}
+
 }
